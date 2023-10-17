2c8d7d803702aedb4da7434303279f3d00e71d58
Adil Redaoui <adil.redaoui28@gmail.com>
2023-10-17 11:00:17 +0100

    change botom

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
         <h1 className={styles.title}>your best online shop destination!</h1>
         <p className={styles.description}>Discover Your Ultimate Online Shopping Destination! Find the finest products and unbeatable deals at your fingertips. Shop with confidence and convenience.
         </p>
-        <button className={styles.button}>Shop new</button>
+        <button className={styles.button}>Shop new on my store</button>
       </div>
       <div className={styles.col}>
         <Image className={styles.img} src={hero} alt='adil hero'/>
